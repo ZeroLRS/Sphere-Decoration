39d055e88994d08b06540d3825ebc175244f5cbe
Lucas Spiker <lucas.spiker@mymail.champlain.edu>
2019-09-13 12:13:54 -0400

    Inital commit. Setting up keyframe timer and recording.

---
+++ Assets/Scripts/ObjectRecorder.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Keyframe
+{
+    public float timestamp;
+    public Vector3 location, rotation, scale;
+}
+
+public class ObjectRecorder : MonoBehaviour
+{    
+    [SerializeField]
+    private List<Keyframe> keyframes;
+
+    [SerializeField]
+    private int keyframeIndex = 0;
+    private float currentFrameDuration;
+
+    public PlaybackDirection playbackDirection;
+    public PlaybackEndBehaviour endBehaviour;
+    public float currentPlaytime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (keyframes.Count > 1)
+        {
+            currentFrameDuration = keyframes[1].timestamp - keyframes[0].timestamp;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPlaytime += Time.deltaTime * (int)playbackDirection;
+
+        // 1) Stopped                                             *
+        // 2) Forward                                             
+        //  a) Has not exceeded duration, do not change keyframe  *
+        //  b) Exceeded duration, procced to next frame           *
+        //   i) If last frame, do ending behaviour                *
+        // 3) Reverse                                             
+        //  a) Has not gone below zero, do not change keyframe    *
+        //  b) Below zero, procced to next (previous) frame       *
+        //   i) If first frame, do ending behaviour               *
+    }
+
+    public void RecordFrameNow()
+    {
+        Keyframe newFrame;
+        newFrame.timestamp = Time.time;
+        newFrame.location = transform.position;
+        newFrame.rotation = transform.rotation.eulerAngles;
+        newFrame.scale = transform.localScale;
+
+        keyframes.Add(newFrame);
+    }
+
+    public void SortKeyFrames()
+    {
+        // Sort keyframes by time
+    }
+}
---
+++ Assets/Scripts/ObjectRecordingManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum PlaybackDirection
+{
+    REVERSE = -1,
+    STOP,
+    FORWARD,
+    count
+}
+
+[System.Serializable]
+public enum PlaybackEndBehaviour
+{
+    STOP = 0,
+    LOOP, 
+    PING_PONG,
+    count
+}
+
+public class ObjectRecordingManager : MonoBehaviour
+{
+    private List<ObjectRecorder> recorders;
+
+    public bool recording = false;
+
+    public string saveFile = "objectRecordings.txt";
+
+    public float targetFrameRate = 0.0f;
+    private float targetTimeStep;
+
+    public float elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  Our timestep in the inverse of our target framerate
+        targetTimeStep = 1 / targetFrameRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (recording)
+        {
+            // Add the time since the last update to our elapsed time
+            elapsedTime += Time.deltaTime;
+
+            // If enough time has elapsed for us to make a new keyframe, record one
+            while (elapsedTime >= targetTimeStep)
+            {
+                // Record a new keyframe in each recorder
+                foreach (ObjectRecorder recorder in recorders)
+                {
+                    recorder.RecordFrameNow();
+                }
+
+                // Now that we've recorded, wait again until we've excceded our timestep
+                elapsedTime -= targetTimeStep;
+            }
+        }
+    }
+}
